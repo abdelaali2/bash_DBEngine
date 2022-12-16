b388d695fab2a2fb8762b678ac7171f7e8244183
Omar Hesham - VM <mromarhesham1999@gmail.com>
2022-12-17 01:19:55 +0200

    main Menu done

--- dbms.sh
+++ dbms.sh
@@ -14,49 +14,85 @@ case $choice in
 	CreateDB
 ;;
 "List Databases")
-	echo "d"
+	listDatabases
 ;; 
 "Connect To Databases")
 	connectToDB
 ;;
 "Rename databases")
-	echo "a"
+	renameDB
 ;;
 "Delete Database")
-	echo "g"
+	deleteDB
 ;;
 "Exit")
  exit
 ;;
 *)
-echo "not found"
+echo -e "Please choose from the options available!\n"
+mainMenu
 esac
 done
 }
 
 function CreateDB(){
 	read -p "Please Enter database name: " dbName
-	mkdir ./dbms/${dbName} 2>> error.text
+	mkdir ./dbms/"${dbName}" 2>> error.text
 	if [ $? -eq 0 ]
 	then
-	echo "The database is created"
+	echo -e "The database is created\n"
 	else
-	echo "The database is already exist" 
+	echo -e "The database is already exist\n" 
 	fi
 }
 
 function connectToDB(){
 	read -p "Please Enter database name: " dbName
-	cd ./dbms/${dbName} 2>> error.text
+	cd ./dbms/"${dbName}" 2>> error.text
 	if [ $? -eq 0 ]
 	then
-	echo "The Database is selected Successfully"
+	echo -e "The Database is selected Successfully\n"
 	#tablesMenu
 	else
 	echo -e "The database was not found\n" 
 	mainMenu
 	fi
 }
+
+function listDatabases(){
+	clear
+	ls ./dbms
+	echo -e "\n"
+	mainMenu
+}
+
+function renameDB(){
+	read -p "Enter database name: " oldDB
+	read -p "Enter the new database name: " newDB
+	mv ./dbms/"${oldDB}" ./dbms/"${newDB}" 2>> error.text
+	
+	if [ $? -eq 0 ]
+	then
+	echo -e "Database renamed\n"
+	else
+	echo -e "Database renamed failed\n"
+	fi
+	mainMenu
+}
+
+function deleteDB(){
+	read -p "Enter database name: " name
+	rm -r ./dbms/"${name}" 2>> error.text
+
+	if [ $? -eq 0 ]
+	then
+	echo -e "Database is deleted\n"
+	else
+	echo -e "Database is not found\n"
+	fi
+	mainMenu
+}
+
 
 
 
