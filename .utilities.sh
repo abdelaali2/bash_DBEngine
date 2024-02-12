a1817e3d9440832a8edb9937dfe452b9ee3508b9
Ibrahim Badr <ibrahim.badreldin@spectro.net>
2024-02-12 13:48:31 +0200

    utilities file updated

--- .utilities.sh
+++ .utilities.sh
@@ -4,9 +4,18 @@ function readInput() {
     echo "${input// /_}"
 }
 
-function validator() {
+function textValidator() {
     local input=$1
     if [[ $input == +([a-zA-Z]*) ]]; then
+        echo true
+    else
+        echo false
+    fi
+}
+
+function numberValidator() {
+    local input=$1
+    if [[ $input == +([1-9]) ]]; then
         echo true
     else
         echo false
