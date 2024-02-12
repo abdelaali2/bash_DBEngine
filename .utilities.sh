a0b1b0fe0f7449794989054b281d7db13c62387d
Ibrahim Badr <ibrahim.badreldin@spectro.net>
2024-02-12 14:01:48 +0200

    validation utilities updated

--- .utilities.sh
+++ .utilities.sh
@@ -6,18 +6,18 @@ function readInput() {
 
 function textValidator() {
     local input=$1
-    if [[ $input == +([a-zA-Z]*) ]]; then
-        echo true
+    if [[ $input == ^[a-zA-Z][a-zA-Z0-9]*$ ]]; then
+        reutrn 0
     else
-        echo false
+        reutrn 1
     fi
 }
 
 function numberValidator() {
     local input=$1
     if [[ $input == +([1-9]) ]]; then
-        echo true
+        reutrn 0
     else
-        echo false
+        reutrn 1
     fi
 }
